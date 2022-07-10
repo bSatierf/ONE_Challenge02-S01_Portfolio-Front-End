b387b6bfcbe307a1942fa61f834e168211b69af1
bSatierf <balbino.fn@gmail.com>
2022-07-09 23:03:11 -0300

    add validação textarea

--- assets/JS/validate.js
+++ assets/JS/validate.js
@@ -1,4 +1,4 @@
-//Seu JavaScript de validação aqui
+//  Seu JavaScript de validação aqui
 
 // Pergar elementos do html
 
@@ -6,37 +6,7 @@ const submitBtn = document.querySelector('.formcontato__form');
 const nomeInput = document.querySelector('#nome');
 const emailInput = document.querySelector('#email');
 const assuntoInput = document.querySelector('#assunto');
-const exclamationIcon = document.querySelector('.exclamation');
-const checkIcon = document.querySelector('.check');
-
-function validaDados() {
-  const nome = nomeInput.value;
-  const email = emailInput.value;
-  const assunto = assuntoInput.value;
-
-  if (nome === '') {
-    adicionaErro(nomeInput, 'O campo nome é obrigatório!');
-    nomeInput.focus();
-    console.log('aquiii');
-  } else if (nome !== '') {
-    adicionaSucesso(nomeInput);
-    if (email === '') {
-      adicionaErro(emailInput, 'O campo email é obrigatório!');
-      console.log('aqui');
-      emailInput.focus();
-    } else if (!validaEmail(email)) {
-      adicionaErro(emailInput, 'Por favor, insira um email válido!');
-    } else {
-      adicionaSucesso(emailInput);
-      console.log('opa');
-      if (assunto === '') {
-        adicionaErro(assuntoInput, 'O campo assunto é obrigatório!');
-      } else if (assunto !== '') {
-        adicionaSucesso(assuntoInput);
-      }
-    }
-  }
-}
+const mensagemInput = document.querySelector('#mensagem');
 
 function validaEmail(email) {
   return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
@@ -46,7 +16,9 @@ function adicionaErro(input, mensagem) {
   const formControl = input.parentElement;
   const small = formControl.querySelector('small');
   const exclamation = formControl.querySelector('.exclamation');
+  const check = formControl.querySelector('.check');
 
+  check.classList.remove('success');
   small.innerHTML = mensagem;
   input.classList.add('error');
   exclamation.classList.add('error');
@@ -66,8 +38,42 @@ function adicionaSucesso(input) {
   check.classList.add('success');
 }
 
+function validaDados() {
+  const nome = nomeInput.value;
+  const email = emailInput.value;
+  const assunto = assuntoInput.value;
+  const mensagemContato = mensagemInput.value;
+
+  if (nome === '') {
+    adicionaErro(nomeInput, 'O campo nome é obrigatório!');
+    nomeInput.focus();
+  } else if (nome !== '') {
+    adicionaSucesso(nomeInput);
+    if (email === '') {
+      adicionaErro(emailInput, 'O campo email é obrigatório!');
+      emailInput.focus();
+    } else if (!validaEmail(email)) {
+      adicionaErro(emailInput, 'Por favor, insira um email válido!');
+      emailInput.focus();
+    } else {
+      adicionaSucesso(emailInput);
+      if (assunto === '') {
+        adicionaErro(assuntoInput, 'O campo assunto é obrigatório!');
+        assuntoInput.focus();
+      } else if (assunto !== '') {
+        adicionaSucesso(assuntoInput);
+        if (mensagemContato === '') {
+          adicionaErro(mensagemInput, 'O campo mensagem é obrigatório!');
+          mensagemInput.focus();
+        } else if (mensagemContato !== '') {
+          adicionaSucesso(mensagemInput);
+        }
+      }
+    }
+  }
+}
+
 submitBtn.addEventListener('submit', (e) => {
   e.preventDefault();
   validaDados();
-  console.log('Olá, Mundo!');
 });
